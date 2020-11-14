9c87ed23bfe6336c22bafef050b40aff0bc71d5c
begeiste <begeiste@gmail.com>
2020-11-13 16:03:37 -0800

    Added the missing closing )

--- CaseVoided/CASE_UNVOID.groovy
+++ CaseVoided/CASE_UNVOID.groovy
@@ -3,7 +3,7 @@ logger.debug("unvoid case ${_case}");
 
 //voided = _case.dispositionType == '2100' || _case.collect("roaMessages[message=='Case is Voided']").notEmpty;
 //IR546286 Charlton added on 9/8/2020
-voided = (_case.dispositionType == '2100' || _case.collect("roaMessages[message=='Case is Voided']")).notEmpty;
+voided = (_case.dispositionType == '2100') || _case.collect("roaMessages[message=='Case is Voided']").notEmpty;
 if (!voided) {
   addError("Case ${_case.caseNumber} is currently not void");
   return;
@@ -66,7 +66,7 @@ if (_hearingToRestore) {
       hearing.resultDate = null;
       hearing.eventStatus = "EVENT";
       hearing.saveOrUpdate();
-
+   
     for (roa in hearing.collect("relatedRoaMessages[message.toLowerCase().contains('vacate')]")) {
       logger.debug("mark roa ${roa.entityIdAndTitle} deleted");
       roa.la_deleted = true;
